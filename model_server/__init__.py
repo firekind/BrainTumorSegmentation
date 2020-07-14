39091eb032e4f8368f265592bc95f687f65bdc39
firekind <shyamant.achar@gmail.com>
2020-07-14 12:32:42 +0530

    fixed stuff

--- model_server/__init__.py
+++ model_server/__init__.py
@@ -97,7 +97,7 @@ def process_request(dir_path: Path, **kwargs) -> Union[np.ndarray, Tuple[Path, s
     # saving result
     if kwargs.get("save_output", True):
         if kwargs.get("as_numpy", True):
-            logger.info("Saving as numpy array")
+            logger.info("Saving as numpy array...")
             np.save(dir_path / res_file, overlaid)
             res_file += ".npy"
         else:
